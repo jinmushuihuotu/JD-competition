c4ada32461338b42213c2e9caf52b289bf61b5f7
Bahn <35649674+lzqzbh@users.noreply.github.com>
2019-04-28 12:10:50 +0800

    直方图

--- temp.py
+++ temp.py
@@ -1,46 +1,59 @@
 # -*- coding: utf-8 -*-
 """
-Spyder Editor
-
-This is a temporary script file.
+Bahn
+数据清洗，绘制直方图
 """
-# 按人和时间双排序
-# 时间转换
+
 import time
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-def time_transform(times):
+
+def data_trans(data):
+    def time_transform(times):
+    if len(times) == 21:
+        times = times[:-2]     
     ts = time.mktime(time.strptime('{}'.format(times),
-                                  "%Y/%m/%d : %H:%M:%S"))
+         "%Y-%m-%d %H:%M:%S")) - 1517414400.0
     return ts
 
-data = pd.read_csv("data/jdata/jdata_action.csv")
-data = data.iloc[100000,:]
+    data = pd.read_csv("data/jdata/jdata_action.csv")
+    data.iloc[:, 2] =  data.iloc[:, 2].apply(time_transform)
+    data = data.sort_values(by = ["user_id", "action_time"])
+    return data
+# 把时间换成时间戳，并按用户ID和时间戳排序
 
-i = 0
-length = len(data)
-nun_user = None
-temp1 = {}
-buy_dauer = np.array([])
+def get_hin(data):
+    i = 0
+    length = len(data)
+    nun_user = None
+    temp1 = {}
+    buy_dauer = np.array([])
+    
+    while i < length:
+        if data.iloc[i, 0] != nun_user:
+            nun_user = data.iloc[i, 0]
+            temp1 = {}
+            
+        if not data.iloc[i, 1] in temp1.keys():
+            if data.iloc[i, 4] in (1, 3, 5):
+                temp1[data.iloc[i, 1]] = data.iloc[i, 2]
+            elif data.iloc[i, 4] == 2:
+                buy_dauer = np.append(buy_dauer, 0)
+        
+        else:
+            if data.iloc[i, 4] == 2:
+                die_time = data.iloc[i, 2] - temp1[data.iloc[i, 1]]
+                die_time = int(die_time/86400) + 1
+                buy_dauer = np.append(buy_dauer, die_time)
+                del temp1[data.iloc[i, 1]]
+        
+        i += 1
+    
+    return buy_dauer
 
-while i < length:
-    if data.iloc[i, 0] != nun_user:
-        nun_user = data.iloc[i, 0]
-        temp1 = {}
-    if not data.iloc[i, 1] in temp1.keys():
-        if data.iloc[i, 4] in (1, 3, 5):
-            temp1[data.iloc[i, 1]] = data.iloc[i, 2]
-        elif data.iloc[i, 4] == 2:
-            np.append(buy_dauer, 0)
-    else:
-        if data.iloc[i, 4] == 2:
-            die_time = data.iloc[i, 2] - temp1[data.iloc[i, 1]]
-            die_time = int(die_time/86400)
-            np.append(buy_dauer, die_time)
-            del temp1[data.iloc[i, 1]]
-
+buy_dauer = get_hin(data)
 # plot
-plt.hist(buy_dauer, bins=20, color='green')
+plt.hist(buy_dauer, bins=300, color='green')
 plt.show()
