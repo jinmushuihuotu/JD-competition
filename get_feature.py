42c36c692f757847f9c905460e267b0d94d35add
Bahn <35649674+lzqzbh@users.noreply.github.com>
2019-04-29 22:04:34 +0800

    提取特征，初步完成

--- get_feature.py
+++ get_feature.py
@@ -15,11 +15,14 @@ action_path = "newdata.csv"
 comment_path = "data/jdata/jdata_comment.csv"
 product_path = "data/jdata/jdata_product.csv"
 user_path = "data/jdata/jdata_user.csv"
+shop_path = "data/jdata/jdata_shop.csv"
+
+
 
 
 class windows():
     
-    def __init__(self, start_date, mid_date, end_date, subset = 1000000):
+    def __init__(self, start_date, mid_date, end_date, subset = 100000):
         '''
         start_date：窗口开始时间
         mid_date：窗口中段时间
@@ -27,6 +30,18 @@ class windows():
         subset：取子样本数量
         '''
         self.subset = subset
+        # 商品特征
+        self.fets1 = ['sku_id', 'cate', 'brand', 'market_time', 'shop_id']
+        # 店铺特征
+        self.fets2 = ['shop_id', 'fans_num', 'vip_num', "shop_reg_tm",
+                    'shop_score']
+        # 用户特征
+        self.fets3 = ["user_id", "age", "sex", "sex", "user_reg_tm",
+                       "user_reg_tm",
+                       "user_lv_cd", "city_level", "city_level", "province",
+                       "province", "city", "city", "county"]
+        
+        
         self.start_date = self.time_transform(start_date)
         self.mid_date = self.time_transform(mid_date)
         self.end_date = self.time_transform(end_date)
@@ -36,6 +51,7 @@ class windows():
         self.actions2 = self.get_actions(self.mid_date,
                                     self.end_date)
         self.feats = self.get_action_feat()
+        self.get_feature_product_shop()
         
     def time_transform(self, times):
         '''
@@ -140,20 +156,92 @@ class windows():
 
         return actions
     
-    
-'''
-def time_transform(times):
-# 转换时间的函数
-# 文本"%Y-%m-%d %H:%M:%S" 转换为时间戳
-    
-    if len(times) == 21:
-        times = times[:-2]
+                     
+    def get_product_shop(self):
+        
+       
+        # sku_id对应商品信息
+        dump_path = './cache/basic_product.pkl'
+        if os.path.exists(dump_path):
+            product = pickle.load(open(dump_path, "rb"))
+        else:
+            product = pd.read_csv(product_path)
+            product = product.dropna()
+            product["market_time"] = product["market_time"].map(
+                    lambda x: self.time_transform(x))
             
-    ts = time.mktime(time.strptime('{}'.format(times),
-         "%Y-%m-%d %H:%M:%S")) - 1517414400.0 #2018-02-01
-    return ts
+            product = product[self.fets1]
+            pickle.dump(product, open(dump_path, 'wb'))
+        
+        ps_dict = product.set_index('sku_id').T.to_dict('list')
+        ps_dict = defaultdict(lambda :np.full([len(self.fets1) - 1],
+                                               np.nan), ps_dict)
+        # shop_id对应店铺信息
+        dump_path = './cache/basic_shop.pkl'
+        if os.path.exists(dump_path):
+            shop = pickle.load(open(dump_path, "rb"))
+        else:
+            shop = pd.read_csv(shop_path)
+            shop = shop.dropna()
+            shop["shop_reg_tm"] = shop["shop_reg_tm"].map(
+                    lambda x: self.time_transform(x))
+            shop = shop[self.fets2]
+            pickle.dump(shop, open(dump_path, 'wb'))
+            
+        sp_dict = shop.set_index('shop_id').T.to_dict('list')
+        sp_dict = defaultdict(lambda :np.full([len(self.fets2) - 1],
+                                               np.nan), sp_dict)
+        
+        # user_id对应用户信息
+        dump_path = './cache/basic_user.pkl'
+        if os.path.exists(dump_path):
+            user = pickle.load(open(dump_path, "rb"))
+        else:
+            user = pd.read_csv(user_path)
+            user = user.dropna()
+            user["user_reg_tm"] = user["user_reg_tm"].map(
+                    lambda x: self.time_transform(x))
+            age_df = pd.get_dummies(user["age"], prefix="age")
+            sex_df = pd.get_dummies(user["sex"], prefix="sex")
+            user_lv_df = pd.get_dummies(user["user_lv_cd"], prefix="user_lv_cd")
+            user = pd.concat([user[self.fets3], age_df, sex_df, user_lv_df], axis=1)
+            self.fets3 = list(user)
+            pickle.dump(user, open(dump_path, 'wb'))
+            
+        us_dict = user.set_index('user_id').T.to_dict('list')
+        us_dict = defaultdict(lambda :np.full([len(self.fets3) - 1],
+                                               np.nan), us_dict)
+        
+        return ps_dict, sp_dict, us_dict
+    
+    
+    def get_feature_product_shop(self):
+        
+        ps_dict, sp_dict, us_dict = self.get_product_shop()
+        
+        for i in range(len(self.fets1) - 1):
+            self.feats[self.fets1[i + 1]] = self.feats["sku_id"].map(lambda x:
+                    ps_dict[x][i])
+        
+            
+        for i in range(len(self.fets2) - 1):
+            self.feats[self.fets2[i + 1]] = self.feats["sku_id"].map(lambda x:
+                    sp_dict[ps_dict[x][-1]][i])
+            
+        for i in range(len(self.fets3) - 1):
+            self.feats[self.fets3[i + 1]] = self.feats["user_id"].map(lambda x:
+                    us_dict[x][i])
+           
+            
+        self.feats.pop("")
+        #self.feats = self.feats.dropna()
+        
+test = windows("2018-02-15 00:00:00",
+                           "2018-02-20 00:00:00",
+                           "2018-03-01 00:00:00")
+test.feats.to_csv("expm.csv", index = False)
 
-
+'''
 数据排序部分
 data = pd.read_csv(action_path)
 data.iloc[:, 2] =  data.iloc[:, 2].map(time_transform)
@@ -186,131 +274,4 @@ def get_hin(data):
                 del temp1[data.loc[i, "sku_id"]]
         i += 1
     return buy_dauer
-
-
-def get_action_feat(actions1, actions2):
-    # 目标变量、浏览时长和浏览次数
-    
-    nun_user = actions1.loc[actions1.index[0], "user_id"]
-    temp1 = {} #初次浏览时间
-    temp2 = {} #末次浏览时间
-    temp3 = defaultdict(lambda: 0) #总浏览次数
-    view_dauer = np.array([])
-    view_times = np.array([], dtype = int)
-    user_id = np.array([], dtype = int)
-    sku_ids = np.array([], dtype = int)
-    for i in actions1.index:
-        if actions1.loc[i, "user_id"] != nun_user:
-            skus = list(temp1.keys())
-            for sku in skus:
-                dauer = temp2[sku] - temp1[sku]
-                dauer = int(dauer/86400) + 1    
-                view_dauer = np.append(view_dauer, dauer)
-                view_times = np.append(view_times, temp3[sku])
-                sku_ids = np.append(sku_ids, sku)
-                user_id = np.append(user_id, nun_user)   
-            nun_user = actions1.loc[i, "user_id"]
-            temp1 = {}
-            temp2 = {}
-            temp3 = defaultdict(lambda: 0)
-        sku_id = actions1.loc[i, "sku_id"]
-        temp3[sku_id] += 1
-        temp2[sku_id] = actions1.loc[i, "action_time"]
-        if sku_id not in temp1.keys():
-            temp1[sku_id] = actions1.loc[i, "action_time"]
-        
-    df = pd.DataFrame({"user_id" : user_id,
-                      "sku_id" : sku_ids,
-                      "view_dauer" : view_dauer,
-                      "view_times" : view_times})
-    tar = np.array([], dtype = int)
-    for i in df.index:
-        has_2 = np.array(actions2[(actions2["user_id"] == 
-                         df.loc[i, "user_id"]) &
-                         (actions2["sku_id"] == 
-                         df.loc[i, "sku_id"])]["type"])
-        if 2 in has_2:
-            tar = np.append(tar, 1)
-        else:
-            tar = np.append(tar, 0)
-    df.insert(0, "tar", tar)
-    return df
-
-def get_basic_user_feat():
-  
-        获取用户基本资料（年龄、性别，用户等级）
-        所有特征['user_id', 'age', 'sex', 'user_reg_tm', 'user_lv_cd', 'city_level',
-        'province', 'city', 'county']
-    
-    
-    dump_path = 'cache/basic_user.pkl'
-    if os.path.exists(dump_path):
-        user = pickle.load(open(dump_path, "rb"))
-    else:
-        user = pd.read_csv(user_path, encoding='gbk')
-        age_df = pd.get_dummies(user["age"], prefix="age")
-        sex_df = pd.get_dummies(user["sex"], prefix="sex")
-        user_lv_df = pd.get_dummies(user["user_lv_cd"],
-               prefix="user_lv_cd")
-        user = pd.concat([user['user_id'],
-            age_df, sex_df, user_lv_df], axis=1)
-        pickle.dump(user, open(dump_path, 'wb'))
-
-    return user
-
-
-def get_basic_product_feat():
-
-    dump_path = './cache/basic_product.pkl'
-    if os.path.exists(dump_path):
-        product = pickle.load(open(dump_path, "rb"))
-    else:
-        product = pd.read_csv(product_path)
-        product["market_time"] = product["market_time"].map(
-                lambda x: int(time_transform(x)/86400) + 1)
-        product = product[['sku_id', 'cate',
-                                     'brand', 'market_time']]
-        pickle.dump(product, open(dump_path, 'wb'))
-    return product
-
-def get_actions(start_date, end_date):
-    """
-    :param start_date:
-    :param end_date:
-    :return: actions: pd.Dataframe
-    """
-    start_date = time_transform(start_date)
-    end_date = time_transform(end_date)
-    dump_path = './cache/all_action_%s_%s.pkl' % (start_date, end_date)
-    if os.path.exists(dump_path):
-        actions = pickle.load(open(dump_path, "rb"))
-    else:
-        actions = pd.read_csv(action_path).iloc[0:5000000,:]
-        actions = actions[(actions["action_time"] >= start_date) &
-                          (actions["action_time"] < end_date)]
-        pickle.dump(actions, open(dump_path, 'wb'))
-    return actions
-
-
-def get_accumulate_action_feat(start_date, mid_date, end_date):
-# 获取一个窗口内的累计特征
-    dump_path = './cache/action_accumulate_%s_%s_%s.pkl' % (start_date,
-                                                mid_date, end_date)
-    if os.path.exists(dump_path):
-        actions_feat = pickle.load(open(dump_path, "rb"))
-    else:
-        actions1 = get_actions(start_date, mid_date)
-        actions2 = get_actions(mid_date, end_date)
-        actions_feat = get_action_feat(actions1, actions2)
-        #pickle.dump(actions_feat, open(dump_path, 'wb'))
-    return actions_feat
-test = get_accumulate_action_feat("2018-02-04 00:00:00",
-                           "2018-02-05 12:00:00",
-                           "2018-02-05 23:59:00")
-
-
-buy_dauer = get_hin(data)
-# plot
-plt.hist(buy_dauer, bins=300, color='green')
-plt.show()
 '''
